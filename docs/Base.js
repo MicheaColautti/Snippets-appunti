c521994db012d6e4f8dfc72884516da691b299ab
Michea Colautti <michea.colautti@samtrevano.ch>
2022-04-28 21:15:20 +0200

    bpoh

--- docs/Base.js
+++ docs/Base.js
@@ -3,5 +3,12 @@ window.addEventListener("devicemotion", handleMotion, true);
 function handleMotion(event) {
     var acc = event.acceleration.z;
     document.getElementById("test").innerHTML = acc;
+    alert(acc);
 
+}
+
+function getZ() {
+
+
+    alert(event.acceleration.z);
 }
