f1540a941fa0d6ca59347a7e19516d6596cf322a
Michea Colautti <michea.colautti@samtrevano.ch>
2022-04-28 21:26:01 +0200

    prova permission

--- docs/Base.js
+++ docs/Base.js
@@ -14,3 +14,23 @@ function getZ() {
     alert("succhiamelo: " + event.acceleration.z);
     //alert(event.acceleration.z);
 }
+
+function permission() {
+    if (typeof(DeviceMotionEvent) !== "undefined" && typeof(DeviceMotionEvent.requestPermission) === "function") {
+        // (optional) Do something before API request prompt.
+        DeviceMotionEvent.requestPermission()
+            .then(response => {
+                // (optional) Do something after API prompt dismissed.
+                if (response == "granted") {
+                    window.addEventListener("devicemotion", (e) => {
+                        // do something for 'e' here.
+                    })
+                }
+            })
+            .catch(console.error)
+    } else {
+        alert("DeviceMotionEvent is not defined");
+    }
+}
+const btn = document.getElementById("request");
+btn.addEventListener("click", permission);
